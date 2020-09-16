92e3aba609848fe6cebb41a7971959223e4899bd
john157157 <john157157@hotmail.com>
2020-09-16 09:16:45 -0500

    message

---
+++ calc.py
@@ -0,0 +1,31 @@
+# I have now elevated this to jpSource/jp_slices.py
+
+
+text = '0123456789'
+
+print(text[0]) # prints 0
+
+print(text[1:]) # prints 123456789
+
+print(text[6:]) # prints 6789
+
+
+
+print(text[-1]) # prints 9
+
+print(text[-7]) # prints 3
+
+print(text[-3:]) # prints 789
+
+
+
+print(text[3:5]) # prints 34
+
+print(text[1:-1]) # prints 12345678
+
+print(text[:-1]) # prints 012345678
+
+print(text[:-2]) # prints 01234567
+
+
+
